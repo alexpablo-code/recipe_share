95257d93080bdb34d225f7082144b515d08d82b4
Rony Pablo <ronyalexis0@gmail.com>
2022-12-09 20:15:01 -0800

    again

--- flask_app/__init__.py
+++ flask_app/__init__.py
@@ -5,5 +5,5 @@ app = Flask(__name__)
 app.secret_key = "secret recipes"
 
 
-logging.basicConfig(filename='errors.log', level=logging.INFO)
+logging.basicConfig(filename='errors.log', level=logging.DEBUG)
 
