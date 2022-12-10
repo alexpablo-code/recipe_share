6feb319d82e7b283de95a1edcf5914dfc992d559
Rony Pablo <ronyalexis0@gmail.com>
2022-12-09 20:47:49 -0800

    FIX3

--- flask_app/__init__.py
+++ flask_app/__init__.py
@@ -5,5 +5,5 @@ import logging
 app.secret_key = "secret recipes"
 
 
-logging.basicConfig(filename='errors.log', level=logging.DEBUG)
+logging.basicConfig(filename='errors.log', level=logging.NOTSET)
 
